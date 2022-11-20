aaac7b9e8dd8e6064e0278e0ad4f4a1b7892250b
wechu07 <wechu017@gmail.com>
2022-11-20 12:30:16 +0300

    react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,32 @@
+import React from "react";
 import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import { 
+  BrowserRouter as Router, 
+  Switch, 
+  Route 
+} from 'react-router-dom';
 
 const App = () => {
-  return <Home/>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/products">
+          <ProductList/>
+        </Route>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
