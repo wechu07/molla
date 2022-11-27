6f30d522c5bf0ccc8e3b8315ff3c07032324b17a
wechu07 <wechu017@gmail.com>
2022-11-28 00:04:35 +0300

    config

--- client/.babelrc.js
+++ client/.babelrc.js
@@ -1,23 +1,8 @@
 const plugins = [
-    [
-      'babel-plugin-import',
-      {
-        libraryName: '@mui/material',
-        libraryDirectory: '',
-        camel2DashComponentName: false,
-      },
-      'core',
-    ],
-    [
-      'babel-plugin-import',
-      {
-        libraryName: '@mui/icons-material',
-        libraryDirectory: '',
-        camel2DashComponentName: false,
-      },
-      'icons',
-    ],
-  ];
-  
-  module.exports = { plugins };
-  
+  [
+    'babel-plugin-direct-import',
+    { modules: ['@mui/material', '@mui/icons-material'] },
+  ],
+];
+
+module.exports = { plugins };
